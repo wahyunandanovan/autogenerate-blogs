da608b2d169737dbbe733aea052cd331e8e5405a
nurhamsah <nur.hamsah.cash@gmail.com>
2022-12-26 16:52:04 +0700

    tess

--- sections/home/Recomendation.tsx
+++ sections/home/Recomendation.tsx
@@ -1,96 +1,93 @@
-import React from "react";
-import SectionContainer from "../../components/SectionContainer";
-import Cover from "../../components/Cover";
-import Image from "next/image";
-import Chip from "../../components/Chip";
-import { AiOutlineRight, AiOutlineLeft, AiOutlineHeart, AiOutlineEye } from "react-icons/ai";
+import React from 'react';
+import SectionContainer from '../../components/SectionContainer';
+import Cover from '../../components/Cover';
+import Image from 'next/image';
+import Chip from '../../components/Chip';
+import { AiOutlineRight, AiOutlineLeft, AiOutlineHeart, AiOutlineEye } from 'react-icons/ai';
 //UTILITY
-import useFetch from "../../hooks/useFetch";
-import moment from "moment";
-import { collection, limit, query } from "firebase/firestore";
-import { getColor } from "../../utils";
-import { palette } from "../../utils/palette";
-import { blurDataURL } from "../../utils/data";
-import { firestore } from "../../utils/firebase";
+import useFetch from '../../hooks/useFetch';
+import moment from 'moment';
+import { collection, limit, query } from 'firebase/firestore';
+import { getColor } from '../../utils';
+import { palette } from '../../utils/palette';
+import { blurDataURL } from '../../utils/data';
+import { firestore } from '../../utils/firebase';
 //STYLE
-import styles from "../../styles/sections/home/recommendation.module.scss";
-
-
+import styles from '../../styles/sections/home/recommendation.module.scss';
 
 function Recommendation() {
+  const q = query(collection(firestore, 'articles'), limit(4));
 
-    const q = query(collection(firestore, "articles"), limit(4));
+  const { data: recomendationData, isLoading } = useFetch(q);
 
-    const { data: recomendationData, isLoading } = useFetch(q)
+  const mapArticles = recomendationData.slice(1, 4);
 
-    const mapArticles = recomendationData.slice(1, 4)
+  const firstArr = recomendationData[0];
 
-    const firstArr = recomendationData[0]
-
-    return (
-        <SectionContainer>
-            <div className={styles.row}>
-                <div>
-                    <h4>
-                        <span>Recomend</span> For You
-                    </h4>
+  return (
+    <SectionContainer>
+      <div className={styles.row}>
+        <div>
+          <h4>
+            <span>Recomend</span> For You
+          </h4>
+        </div>
+        <div className={styles.btn_wrapp}>
+          <button>
+            <AiOutlineLeft size={20} />
+          </button>
+          <button>
+            <AiOutlineRight size={20} />
+          </button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.left_column}>
+          <Cover
+            image={firstArr?.images[0]}
+            category={firstArr?.category?.name.toUpperCase()}
+            avatar={'/images/wahyu.jpg'}
+            chipcolor={getColor(firstArr?.category?.id)}
+            title={firstArr?.title}
+            authorname="Wahyu Nanda"
+            time={moment(String(firstArr?.created_at)).fromNow()}
+            like={firstArr?.like + 20}
+            view={firstArr?.like + 25}
+          />
+        </div>
+        <div className={styles.rigth_column}>
+          {mapArticles?.map((item: any, idx: number) => {
+            const img = item?.images[0];
+            const time = moment(String(item.created_at)).fromNow();
+            return (
+              <div key={idx} className={styles.wrapper_map}>
+                <div className={styles.img_wrapp}>
+                  <Image src={img} alt={`img-alt${img}`} fill placeholder="blur" blurDataURL={blurDataURL} />
+                  <div>
+                    <Chip title={item.category.name.toUpperCase()} backgroundColor={getColor(item.category.id)} />
+                  </div>
                 </div>
-                <div className={styles.btn_wrapp}>
-                    <button>
-                        <AiOutlineLeft size={20} />
-                    </button>
-                    <button>
-                        <AiOutlineRight size={20} />
-                    </button>
+                <div className={styles.detail}>
+                  <h5>{item.title}</h5>
+                  <p>Gafar R ~ {time}</p>
+                  <div className={styles.like_wrapper}>
+                    <div>
+                      <AiOutlineHeart size={20} color={palette.grey} style={{ cursor: 'pointer' }} />
+                      <p>{item.like + 20}</p>
+                    </div>
+                    <div>
+                      <AiOutlineEye size={20} color={palette.grey} style={{ cursor: 'pointer' }} />
+                      <p>{item.view + 25}</p>
+                    </div>
+                  </div>
                 </div>
-            </div>
-            <div className={styles.content}>
-                <div className={styles.left_column}>
-                    <Cover
-                        image={firstArr?.images[0]}
-                        category={firstArr?.category?.name.toUpperCase()}
-                        avatar={'/images/wahyu.jpg'}
-                        chipcolor={getColor(firstArr?.category?.id)}
-                        title={firstArr?.title}
-                        authorname="Wahyu Nanda"
-                        time={moment(String(firstArr?.created_at)).fromNow()}
-                        like={firstArr?.like + 20}
-                        view={firstArr?.like + 25} />
-                </div>
-                <div className={styles.rigth_column}>
-                    {mapArticles?.map((item: any, idx: number) => {
-                        console.log(item)
-                        const img = item?.images[0]
-                        const time = moment(String(item.created_at)).fromNow()
-                        return (
-                            <div key={idx} className={styles.wrapper_map}>
-                                <div className={styles.img_wrapp} >
-                                    <Image src={img} alt={`img-alt${img}`} fill placeholder='blur' blurDataURL={blurDataURL} />
-                                    <div>
-                                        <Chip title={item.category.name.toUpperCase()} backgroundColor={getColor(item.category.id)} />
-                                    </div>
-                                </div>
-                                <div className={styles.detail}>
-                                    <h5>{item.title}</h5>
-                                    <p>Gafar R ~ {time}</p>
-                                    <div className={styles.like_wrapper}>
-                                        <div>
-                                            <AiOutlineHeart size={20} color={palette.grey} style={{ cursor: 'pointer' }} />
-                                            <p>{item.like + 20}</p>
-                                        </div>
-                                        <div>
-                                            <AiOutlineEye size={20} color={palette.grey} style={{ cursor: 'pointer' }} />
-                                            <p>{item.view + 25}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        </SectionContainer>
-    );
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </SectionContainer>
+  );
 }
 
 export default Recommendation;
--- sections/home/Trending.tsx
+++ sections/home/Trending.tsx
@@ -1,26 +1,25 @@
-import React from "react";
-import SectionContainer from "../../components/SectionContainer";
-import Cover from "../../components/Cover";
-import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+import React from 'react';
+import SectionContainer from '../../components/SectionContainer';
+import Cover from '../../components/Cover';
+import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
 //UTILITY
-import useFetch from "../../hooks/useFetch";
-import moment from "moment";
-import { getColor } from "../../utils";
-import { collection, limit, orderBy, query } from "firebase/firestore";
-import { firestore } from "../../utils/firebase";
+import useFetch from '../../hooks/useFetch';
+import moment from 'moment';
+import { getColor } from '../../utils';
+import { collection, limit, orderBy, query } from 'firebase/firestore';
+import { firestore } from '../../utils/firebase';
 //STYLE
-import styles from "../../styles/sections/home/trending.module.scss";
+import styles from '../../styles/sections/home/trending.module.scss';
 
 function Trending() {
-  const q = query(collection(firestore, "articles"), limit(3), orderBy("created_at"));
+  const q = query(collection(firestore, 'articles'), limit(3), orderBy('created_at', 'desc'));
 
-  const { data: trendingData, isLoading } = useFetch(q)
+  const { data: trendingData, isLoading } = useFetch(q);
 
-  const arr1 = trendingData[0]
-  const arr2 = trendingData[1]
-  const arr3 = trendingData[2]
-  const time = (item: any) => moment(String(item?.created_at)).fromNow()
-
+  const arr1 = trendingData[0];
+  const arr2 = trendingData[1];
+  const arr3 = trendingData[2];
+  const time = (item: any) => moment(String(item?.created_at)).fromNow();
 
   return (
     <SectionContainer>
@@ -38,39 +37,47 @@ function Trending() {
             <AiOutlineRight size={20} />
           </button>
         </div>
-
       </div>
       <div className={styles.content}>
         <div className={styles.left_column}>
           <Cover
             trending
             image={arr1?.images[0]}
-            category='ECONOMIC'
-            avatar='/images/wahyu.jpg'
+            category="ECONOMIC"
+            avatar="/images/wahyu.jpg"
             chipcolor={getColor('ECONOMIC')}
-            title='Creative photography ideas from smart devices'
+            title={arr1?.title}
             authorname="Nahyu Nanda"
-            time={time(arr1)} like={200} view={200} />
+            time={time(arr1)}
+            like={200}
+            view={200}
+          />
         </div>
         <div className={styles.rigth_column}>
           <Cover
             trending
             image={arr2?.images[0]}
-            category='ENTERTAINMENT'
-            avatar='/images/wahyu.jpg'
+            category="ENTERTAINMENT"
+            avatar="/images/wahyu.jpg"
             chipcolor={getColor('ENTERTAINMENT')}
-            title='Creative photography ideas from smart devices'
+            title={arr2?.title}
             authorname="Wahyu Nanda"
-            time={time(arr2)} like={200} view={200} />
+            time={time(arr2)}
+            like={200}
+            view={200}
+          />
           <Cover
             trending
             image={arr3?.images[0]}
-            category='SPORT'
-            avatar='/images/wahyu.jpg'
+            category="SPORT"
+            avatar="/images/wahyu.jpg"
             chipcolor={getColor('SPORT')}
-            title='Creative photography ideas from smart devices'
+            title={arr3?.title}
             authorname="Nahyu Nanda"
-            time={time(arr3)} like={200} view={200} />
+            time={time(arr3)}
+            like={200}
+            view={200}
+          />
         </div>
       </div>
     </SectionContainer>
